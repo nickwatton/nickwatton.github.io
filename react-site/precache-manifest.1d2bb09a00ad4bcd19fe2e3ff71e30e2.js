cc7e9594b51bf95c36e4ab8ecb5219366b2f0cad
Nick <nickwatton@gmail.com>
2020-04-08 12:20:51 +0100

    New React site - test build before deployment

---
+++ react-site/precache-manifest.1d2bb09a00ad4bcd19fe2e3ff71e30e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa715d5291815e03382e02aff2c81965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf156bcd56a5ae3f5228",
+    "url": "/static/css/main.903c4084.chunk.css"
+  },
+  {
+    "revision": "e40d801c3ec4a5a0e206",
+    "url": "/static/js/2.f9a49c76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9a49c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf156bcd56a5ae3f5228",
+    "url": "/static/js/main.669c173c.chunk.js"
+  },
+  {
+    "revision": "b71fc92e9b0871852091",
+    "url": "/static/js/runtime-main.b8485ccb.js"
+  }
+]);
